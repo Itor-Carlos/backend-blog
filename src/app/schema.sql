fbe82b389420020b06bfdc96ad25a8b4fe857ad8
Itor Carlos <itor51carlos@gmail.com>
2024-04-20 18:13:49 -0300

    add sql file

---
+++ src/app/schema.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE Challenge;
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE Users (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(100) NOT NULL,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Posts (
+    id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+    titulo VARCHAR(100) NOT NULL,
+    conteudo TEXT NOT NULL,
+    autor_id UUID NOT NULL,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (autor_id) REFERENCES Users(id)
+);
