a592b85e6ce883f0ef7299e9829d7e9213c4e2d6
Itor Carlos <itor51carlos@gmail.com>
2024-04-22 19:19:36 -0300

    changed the Users schema to use Brazil timezone

--- src/app/database/schema.sql
+++ src/app/database/schema.sql
@@ -7,8 +7,9 @@ CREATE TABLE Users (
     nome VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     senha VARCHAR(100) NOT NULL,
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    data_criacao TIMESTAMP WITH TIME ZONE DEFAULT timezone('America/Sao_Paulo'::TEXT, CURRENT_TIMESTAMP)
 );
+
 
 CREATE TABLE Posts (
     id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
