b5e5aa66793283796af681d75de0cd1c7e0b6cf0
Itor Carlos <itor51carlos@gmail.com>
2024-05-12 12:06:43 -0300

    alterado endpoint de cadastro de usuario

--- src/routes.ts
+++ src/routes.ts
@@ -7,7 +7,7 @@ const router = Router();
 
 router.get('/user', UserController.index);
 router.get('/user/:id', UserController.show);
-router.post('/user', UserController.store);
+router.post('/register', UserController.store);
 router.delete('/user/:id', UserController.remove);
 router.put('/user/:id', UserController.update);
 
