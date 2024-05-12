1b253a80dac64361ca82caa656c882b8fce2af9f
Itor Carlos <itor51carlos@gmail.com>
2024-05-12 11:41:55 -0300

    removido rota padrão /

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,6 @@ const PORT = 3000;
 app.use(express.json());
 app.use(router);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Primeira rota!');
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
