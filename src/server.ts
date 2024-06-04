d6e90530eb85696e80eeeb74048dcd41b572f1d8
Itor Carlos <itor51carlos@gmail.com>
2024-06-03 21:25:42 -0300

    feat: add configuracao do cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import express, { Request, Response } from 'express';
 import router from './routes';
 
+const cors = require('cors');
+
 const app = express();
 const PORT = 3000;
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(router);
 
